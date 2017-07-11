f19cbe85ad898e00b22f9e0bddc8de913c7738d8
Brandyn Sullins <woofenwauzer@gmail.com>
2017-07-11 13:31:03 -0400

    foreach added

--- mainforeach.js
+++ mainforeach.js
@@ -11,9 +11,9 @@ function question1 () {
   // Answer:
   let totcost = 0;
   let averageCost = 0;
-  for (i=0; i < data.length; i ++) {
-    totcost += data[i].price;
-  }
+  data.forEach(function(data){
+    totcost += data.price;
+  })
   averageCost = (totcost/data.length);
   console.log(averageCost.toFixed(2));
 }
@@ -24,11 +24,11 @@ function question1 () {
 function question2 () {
   // Answer:
   let specialNums = [];
-  for (i=0; i < data.length; i ++) {
-    if (data[i].price >= 14 && data[i].price <= 18) {
-      specialNums.push(data[i].title);
+  data.forEach(function(data){
+    if (data.price >=14 && data.price <=18) {
+      specialNums.push(data.title);
     }
-  }
+  })
   console.log(specialNums);
 }
 
@@ -37,47 +37,48 @@ function question2 () {
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
   // Answer:
-  for (i=0; i < data.length; i ++){
-    if (data[i].currency_code === "GBP") {
-      console.log(data[i].title);
-      console.log(data[i].price);
+  data.forEach(function(data) {
+    if (data.currency_code === "GBP") {
+      console.log(data.title);
+      console.log(data.price);
     }
-  }
+  })
 }
 
 
-// 4: Display a list of all items who are made of wood.
+// // 4: Display a list of all items who are made of wood.
 function question4 () {
   //Answer:
-    for (i=0; i < data.length; i ++){
-    if (data[i].materials.includes("wood")) {
-      console.log(data[i].title);
-    }
-  }
+    data.forEach(function(data) {
+      if (data.materials.includes("wood")) {
+        console.log(data.title);
+      }
+    })
 }
-
-// 5: Which items are made of eight or more materials?
-//    Display the name, number of items and the items it is made of.
+//
+// // 5: Which items are made of eight or more materials?
+// //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
-  for (i=0; i < data.length; i ++){
-  if (data[i].materials.length >= 8) {
-    console.log(data[i].title);
-    console.log("This is made of " + data[i].materials.length + " different items");
-    console.log(data[i].materials);
-  }
+  data.forEach(function(data) {
+    if (data.materials.length >= 8) {
+      console.log(data.title);
+      console.log("This is made of " + data.materials.length + " different items");
+      console.log(data.materials);
+    }
+  })
 }
-}
-
-
-// 6: How many items were made by their sellers?
-// Answer:
+//
+//
+// // 6: How many items were made by their sellers?
+// // Answer:
 function question6 () {
   // Answer:
-  let handmadeNumber = 0;
-  for (i=0; i < data.length; i++) {
-    if (data[i].who_made === "i_did") {
+  let handmadeNumber = 0
+  data.forEach(function(data) {
+    if (data.who_made === "i_did") {
       handmadeNumber ++;
     }
+  })
   console.log(handmadeNumber + " items were made by their sellers.");
 }
